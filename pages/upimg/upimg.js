44b79597b85a0e1524f54a9f075db448f4b66c1e
lzd19 <69095282+lzd19@users.noreply.github.com>
2021-06-12 16:33:01 +0800

    11

--- pages/upimg/upimg.js
+++ pages/upimg/upimg.js
@@ -1,7 +1,41 @@
 // pages/upimg/upimg.js
 Page({
   data:{
-    AddImg:""
+    AddImg:"",
+    AddVedeo:""
+  },
+  /* 上传文件 */
+  upExcel(){
+    wx.chooseMessageFile({
+      count: 1,
+      type:'all',
+      success(res){
+        console.log("选择成功".res)
+        wx.cloud.uploadFile({
+          cloudPath:'编程.xlsx',
+          filePath:res.tempFiles[0].path,
+          success(res){
+            console.log('上传成功',res)
+          },fail(res){
+            console.log('上传失败',res)
+          }
+        })
+      }
+    })
+  },
+  /* 下载打开文件 */
+  opExcel(){
+    wx.cloud.downloadFile({
+      fileID:'cloud://ce-shi-1o9pp.6365-ce-shi-1o9pp-1301847921/编程.xlsx',
+      success(res){
+        wx.openDocument({
+          filePath:res.tempFilePath,
+          success(res){
+            console.log('打开成功',res)
+          }
+        })
+      }
+    })
   },
   /* 上传图片 */
   upImg(){
@@ -17,6 +51,7 @@ Page({
       }
     })
   },
+
   /* 上传图片到云存储 */
   upLoadImg(lzd){
     let that=this
@@ -35,6 +70,7 @@ Page({
       }
     })
   },
+
   /* 上传视频 */
   upVedio(){
     wx.chooseVideo({
@@ -43,7 +79,17 @@ Page({
       camera: 'back',
       success(res) {
         console.log("选择成功",res.tempFilePath)
-        
+        wx.cloud.uploadFile({
+          cloudPath: 'dege.mp4',
+          filePath: res.tempFilePath, // 文件路径(由于data的链接写死了，此处不会报错，)
+          success: res => {
+            // get resource ID
+            console.log("YES",res)
+          },
+          fail: err => {
+            console.log("NO",err)
+          }
+        })
       }
     })
   },
